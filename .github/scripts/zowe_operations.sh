a251e888b72b10734ff8ebaca391eaaa58656ad0
Igor Crispim <104989162+IgorCrispim@users.noreply.github.com>
2025-09-09 19:32:18 -0300

    Refactor zowe_operations.sh to use TARGET_DIR variable

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,23 +2,22 @@
 # zowe_operations.sh
 
 # Converte o nome de utilizador para minúsculas
-LOWERCASE_USERNAME=$(echo "$ZOWE_CLI_USER" | tr '[:upper:]' '[:lower:]')
+# A variável ZOWE_USER_FOR_SCRIPT é passada pelo ficheiro de workflow
+LOWERCASE_USERNAME=$(echo "$ZOWE_USER_FOR_SCRIPT" | tr '[:upper:]' '[:lower:]')
+TARGET_DIR="/z/$LOWERCASE_USERNAME/cobolcheck"
 
-# Verifica se o diretório existe, cria se não existir
-# O Zowe CLI irá usar as variáveis de ambiente (ZOWE_CLI_HOST, etc.) para a conexão
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+# Os comandos Zowe irão usar os perfis 'default' criados no workflow
+echo "Verificando se o diretório $TARGET_DIR existe..."
+if ! zowe zos-files list uss-files "$TARGET_DIR" &>/dev/null; then
   echo "Diretório não existe. Criando..."
-  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
+  zowe zos-files create uss-directory "$TARGET_DIR"
 else
   echo "Diretório já existe."
 fi
 
-# Faz o upload dos ficheiros
 echo "A fazer o upload dos ficheiros do COBOL Check..."
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive \
-  --binary-files "*.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "$TARGET_DIR" --recursive --binary-files "*.jar"
 
-# Verifica o upload
 echo "Verificando o upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "$TARGET_DIR"
 
