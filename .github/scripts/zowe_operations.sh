8a959ab0e49bde2289d59d0d5c27de2f9a181b2a
Igor Crispim <104989162+IgorCrispim@users.noreply.github.com>
2025-09-09 17:52:57 -0300

    Refactor zowe_operations.sh to use environment variables

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -4,15 +4,11 @@
 # Converte o nome de utilizador para minúsculas
 LOWERCASE_USERNAME=$(echo "$ZOWE_CLI_USER" | tr '[:upper:]' '[:lower:]')
 
-# Define as opções de conexão para usar em todos os comandos Zowe
-# As variáveis (ZOWE_CLI_HOST, etc.) vêm do ficheiro main.yml
-CONN_OPTS="--host $ZOWE_CLI_HOST --port $ZOWE_CLI_PORT --user $ZOWE_CLI_USER --password $ZOWE_CLI_PASSWORD"
-
 # Verifica se o diretório existe, cria se não existir
-# A opção --sshs-fingerprint-check false é para evitar prompts de confirmação de SSH
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $CONN_OPTS --sshs-fingerprint-check false &>/dev/null; then
+# O Zowe CLI irá usar as variáveis de ambiente (ZOWE_CLI_HOST, etc.) para a conexão
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Diretório não existe. Criando..."
-  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" $CONN_OPTS --sshs-fingerprint-check false
+  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
 else
   echo "Diretório já existe."
 fi
@@ -20,9 +16,9 @@ fi
 # Faz o upload dos ficheiros
 echo "A fazer o upload dos ficheiros do COBOL Check..."
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive \
-  --binary-files "*.jar" $CONN_OPTS --sshs-fingerprint-check false
+  --binary-files "*.jar"
 
 # Verifica o upload
 echo "Verificando o upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $CONN_OPTS --sshs-fingerprint-check false
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
 
